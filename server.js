620e956481a69c9e58989a62d77a2ed195791294
htata31 <htata31@gmail.com>
2018-10-30 02:28:07 -0500

    Commit3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/Public'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
