f9f46bec581d4d95c8d1ada144de01cb7d947ec8
htata31 <htata31@gmail.com>
2018-10-30 22:06:02 -0500

    -Added the contact.html and changes done in profile page. -Added custom css files and controllers for the respective pages

---
+++ public/controller/HomeController.js
@@ -0,0 +1,14 @@
+var home_page= angular.module('homepage',[]);
+home_page.controller('homeController', function($scope, $http,$window) {
+    var url=window.location.href;
+    var userName=(url.substr(url.indexOf("?")+1)).replace("%20"," ");
+    console.log(userName);
+    $scope.uname=userName;
+
+    $scope.GoProfile= function () {
+        $window.location.href="profilepage.html?"+$scope.uname;
+    }
+    $scope.GoContact= function () {
+        $window.location.href="contact.html?"+$scope.uname;
+    }
+})
--- public/controller/ProfileController.js
+++ public/controller/ProfileController.js
@@ -1,4 +1,40 @@
 var profile_page= angular.module('Profile',[]);
 profile_page.controller('ProfileController', function($scope, $http,$window) {
+    var url = window.location.href;
+
+    var userName = (url.substr(url.indexOf("?") + 1)).replace("%20", " ");
+    console.log(userName);
+    $scope.uname = userName;
+
+    $scope.GoContact = function () {
+        $window.location.href = "contact.html?" + $scope.uname;
+    }
+    $scope.GoHome = function () {
+        $window.location.href = "Homepage.html?" + $scope.uname;
+    }
+    $scope.p_uname = userName;
+    $scope.Update=function () {
+
+
+        var dataParams = {
+            'username': $scope.username,
+            'firstname': $scope.fname,
+            'lastname':$scope.lname,
+            'number': $scope.number
+        };
+
+
+        $http.get('http://127.0.0.1:8080/update?keywords=', dataParams).then(function (d)
+            {
+                console.log(typeof(d));
+                console.log("length is " + d.data.length);
+
+            }, function (err) {
+                console.log(err);
+            }
+        )
+
+    }
+
 
 })
